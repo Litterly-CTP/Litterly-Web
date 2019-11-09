8f4664264587d5a0ce3562a2112a735855167cf4
Sparkadelic177 <sparkyevangelista@gmail.com>
2019-11-09 10:14:41 -0500

    gitignore

--- Backend/Controllers/index.js
+++ Backend/Controllers/index.js
@@ -1,7 +1,12 @@
 const admin = require('firebase-admin');
 const { config } = require('../credentials.js');
 
+/*
+    This file is going to hold all of the routes 
+    in the controllers folder./
+*/
 
+//firebase init
 admin.initializeApp(config);
 let db = admin.firestore();
 
