4ca27d4b1ac0496dd4437faba1cb5fabcb0ac69c
Sparkadelic177 <sparkyevangelista@gmail.com>
2019-12-06 20:09:09 -0500

    connected google sign in

--- Backend/functions/index.js
+++ Backend/functions/index.js
@@ -1,12 +1,19 @@
 const functions = require('firebase-functions');
-const app = require('express')();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser')
 
+let engine = require("consolidate");
+app.engine("html", engine.mustache);
+app.set("view engine", "html");
+
+app.use(express.static(__dirname + "../../frontend"))
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
 
 //viewed on the meetups scheduler
 app.use('/view_meetup', require('../controllers/view_meetups.js')); //tested
